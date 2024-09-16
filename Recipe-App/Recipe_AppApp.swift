12739bc21b167094755a88cb0205a86e9d36b690
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-09-16 11:27:32 -0700

    Initial Commit

---
+++ Recipe-App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe-App
+//
+//  Created by Lesley Lopez on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
